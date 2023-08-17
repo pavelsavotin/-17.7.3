9bbbfc2181499daf9b35615a8a74ca0aa32924b5
pavel savotin <135619754+pavelsavotin@users.noreply.github.com>
2023-08-17 17:43:20 +0300

    Add files via upload

---
+++ Задание 22.9.1.py
@@ -0,0 +1,28 @@
+def binary_search(array, element, left, right):
+    if left > right:  # если левая граница превысила правую,
+        return False  # элемент отсутствует
+    middle = (right + left) // 2
+    if element == array[middle]:
+        if array[middle - 1] < element and element <= array[middle]:  # если элемент в середине,
+            return middle-1  # возвращаем этот индекс
+        elif element < array[middle]:  # если элемент меньше элемента в середине
+            # ищем в левой половине
+            return binary_search(array, element, left, middle - 1)
+    elif element == array[middle - 1]:  # если элемент меньше элемента в середине
+        # ищем в левой половине
+        return binary_search(array, element, left, middle - 1)
+    else:  # иначе в правой
+        return binary_search(array, element, middle + 1, right)
+
+
+array = list(map(int,input("Введите целые числа в любом порядке, через пробел: ").split()))
+element = int(input("Введите любое положительное целое число: "))
+array = sorted(array)
+left = int(array[0])
+right = int(array[-1])
+# запускаем алгоритм на левой и правой границе
+if element < left or element > right:
+    print('Числа нет в диапазоне')
+else:
+    print(binary_search(array, element, 0, len(array) - 1))
+print(array)
